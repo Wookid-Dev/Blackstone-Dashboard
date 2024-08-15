fd2b888b7bb70351eae69640c4f91a07d2cb6afb
Isaac Sanchez <isaacsanchez016@gmail.com>
2024-08-15 01:19:05 -0600

    chore: modify next config to update home path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/',
+          destination: '/pages',
+        },
+      ]
+    },
+  }
+  
+  export default nextConfig
